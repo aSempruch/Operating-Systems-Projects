9861343f48849a010779b07ee1dde52c81c469b5
orlandosantoni <orlandosantoni@gmail.com>
2018-03-07 19:52:41 -0500

    Moving around

---
+++ code/Asst1/main.c
@@ -0,0 +1,53 @@
+#include "my_pthread_t.h"
+#include <stdio.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+void *test2(void *arg){
+	printf("MAMANCISCO\n");
+	return NULL;
+}
+
+my_pthread_mutex_t mutex;
+
+void *test(void *arg){
+	//ucontext_t * nthread = (ucontext_t*) malloc(sizeof(ucontext_t));
+	//getcontext(nthread);
+	//printf("Calling from stack %d\n", &nthread->uc_stack);
+
+	printf("%s\n", (char*)arg);
+	printf("FRANCISCO\n");
+	my_pthread_t p2;
+	my_pthread_create(&p2, NULL, test2, (void*)"A");
+	printf("got to join\n" );
+	my_pthread_join(p2, NULL);
+	printf("End of Francisco");
+	return NULL;
+}
+
+void *wait(void* arg){
+	//my_pthread_mutex_t* mal;
+	//my_pthread_mutex_init(mal, NULL);
+
+	my_pthread_mutex_lock(&mutex);
+	sleep(10);
+	my_pthread_mutex_unlock(&mutex);
+	return NULL;
+}
+
+int main(){
+	my_pthread_mutex_init(&mutex, NULL);
+	// initialize();
+	my_pthread_t p1, p3;
+	printf("Main begin\n");
+
+	my_pthread_create(&p1, NULL, wait, NULL);
+	my_pthread_create(&p3, NULL, wait, NULL);
+  printf("Got to join\n");
+	my_pthread_join(p1, NULL);
+	my_pthread_join(p3, NULL);
+
+	printf("Main ends\n");
+
+	return 0;
+}
---
+++ code/Asst1/my_pthread_t.h
@@ -0,0 +1,86 @@
+// File:	my_pthread_t.h
+// Author:	Yujie REN
+// Date:	09/23/2017
+
+// name:
+// username of iLab:
+// iLab Server:
+#ifndef MY_PTHREAD_T_H
+#define MY_PTHREAD_T_H
+
+#define _GNU_SOURCE
+
+/* include lib header files that you need here: */
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
+
+typedef uint my_pthread_t;
+
+typedef struct my_pthread_mutex_t my_pthread_mutex_t;
+
+typedef struct threadControlBlock {
+  ucontext_t* thread;
+  my_pthread_t tid;
+  struct threadControlBlock* next;
+  int prior; //Priority, the higher the number the higher the priority
+  struct threadControlBlock* joinQueue;
+  void** joinArg;
+  int time;
+} tcb;
+
+/* mutex struct definition */
+struct my_pthread_mutex_t {
+  int state; //0 is unlocked, 1 is locked
+  tcb* head;
+  pthread_mutexattr_t *attr;
+};
+
+/* define your data structures here: */
+
+// Feel free to add your own auxiliary data structures
+
+
+/* Function Declarations: */
+
+tcb* gettcb();
+int updatePrior(tcb* thread, int prior);
+int removeFromQueue(tcb* thread);
+int updatePrior2(tcb* thread, int prior);
+tcb* getParent(tcb* thread);
+void startScheduler();
+
+/* create a new thread */
+int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg);
+
+/* give CPU pocession to other user level threads voluntarily */
+int my_pthread_yield();
+
+/* terminate a thread */
+void my_pthread_exit(void *value_ptr);
+
+/* wait for thread termination */
+int my_pthread_join(my_pthread_t thread, void **value_ptr);
+
+/* initial the mutex lock */
+int my_pthread_mutex_init(my_pthread_mutex_t *mutex, const pthread_mutexattr_t *mutexattr);
+
+/* aquire the mutex lock */
+int my_pthread_mutex_lock(my_pthread_mutex_t *mutex);
+
+/* release the mutex lock */
+int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex);
+
+/* destroy the mutex */
+int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex);
+
+#endif
+
+/*my_pthread_t.h:85:24: error: conflicting types for ‘my_pthread_create’
+ #define pthread_create my_pthread_create
+                        ^
+my_pthread_t.h:56:5: note: previous declaration of ‘my_pthread_create’ was here
+ int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg);*/
